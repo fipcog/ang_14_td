90cfdad1ab1e8ec77a743b6d3cdff8931d9f7546
Artur <looppinooppi@gmail.com>
2024-04-23 13:16:31 +0300

    feat: todos class

--- src/app/todos/component/todos.component.ts
+++ src/app/todos/component/todos.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TodosService } from '../services/todos.service';
+import { Observable } from 'rxjs';
+import { Todolist } from 'src/app/core/models';
 
 @Component({
   selector: 'td-todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.scss']
 })
-export class TodosComponent {
+export class TodosComponent implements OnInit {
 
+  todos$!: Observable<Todolist[]>
+
+  constructor(private todosService: TodosService) { }
+
+  ngOnInit(): void {
+    this.todos$ = this.todosService.todos$
+
+    this.todosService.getTodos()
+  }
 }
