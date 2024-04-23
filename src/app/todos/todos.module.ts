de51547cbb25458e3460aa5b64b080de899c3083
Artur <looppinooppi@gmail.com>
2024-04-23 15:27:24 +0300

    feat: todo

--- src/app/todos/todos.module.ts
+++ src/app/todos/todos.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { TodosRoutingModule } from './todos-routing.module';
 import { TodosComponent } from './component/todos/todos.component';
 import { HttpClientModule } from '@angular/common/http';
+import { TodoComponent } from './component/todos/todo/todo.component';
 
 
 @NgModule({
   declarations: [
-    TodosComponent
+    TodosComponent,
+    TodoComponent
   ],
   imports: [
     CommonModule,
