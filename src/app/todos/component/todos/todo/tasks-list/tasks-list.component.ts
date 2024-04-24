2fdc7255bedcc52692c3613856f814e26d378d62
Artur <looppinooppi@gmail.com>
2024-04-24 14:20:31 +0300

    feat: created tasks-list & task

--- src/app/todos/component/todos/todo/tasks-list/tasks-list.component.ts
+++ src/app/todos/component/todos/todo/tasks-list/tasks-list.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { Task } from 'src/app/core/models';
+import { TasksService } from 'src/app/todos/services/tasks.service';
 
 @Component({
   selector: 'td-tasks-list',
   templateUrl: './tasks-list.component.html',
   styleUrls: ['./tasks-list.component.scss']
 })
-export class TasksListComponent {
+export class TasksListComponent implements OnInit {
+  @Input() todoId = ''
 
+  tasks$!: Observable<Task[]>
+  newTaskTitle = ''
+
+  constructor(private tasksService: TasksService) { }
+
+  ngOnInit(): void {
+    this.tasksService.getTasks(this.todoId)
+
+    this.tasks$ = this.tasksService.tasks$.pipe(map(
+      dTasks => dTasks[this.todoId]
+    ))
+  }
+
+  addTaskHandler() {
+    if (this.newTaskTitle.length) {
+      this.tasksService.addTask(this.todoId, this.newTaskTitle)
+      this.newTaskTitle = ''
+    } else {
+      alert('Task title should nit be ampty')
+    }
+  }
 }
